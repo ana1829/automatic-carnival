48dd2b29ecde35b40d73d6ff616be94688ea855b
Ana <acarredo@cougarnet.uh.edu>
2022-11-10 15:10:21 -0600

    my first commit

---
+++ Models/Reports.cs
@@ -0,0 +1,11 @@
+﻿namespace ZooWebsite.Models
+{
+    public class Reports
+    {
+        public int Id { get; set; }
+
+        public char FName { get; set; }
+
+        public char LName { get; set; }
+    }
+}
